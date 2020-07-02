cf378c359ce4f5dafb395f53c89bb40215240798
jro <j@ro.vu>
2020-07-02 09:27:46 -0500

    Use constant instead of var

---
+++ HelloWorld/Lesson.swift
@@ -0,0 +1,8 @@
+//
+//  Lesson.swift
+//  HelloWorld
+//
+//  Created by Jonathan Rogivue on 2020-07-02.
+//  Copyright © 2020 JRO. All rights reserved.
+//
+
--- HelloWorld/PhraseIndexView.swift
+++ HelloWorld/PhraseIndexView.swift
@@ -9,9 +9,7 @@
 import SwiftUI
 
 struct PhraseIndexView: View {
-    var items = (1..<100).map { index in
-        "\(index) item"
-    }
+    let items = (1..<100).map { "\($0) item" }
     
     var body: some View {
         ScrollView {
@@ -30,39 +28,4 @@ struct PhraseIndexView_Previews: PreviewProvider {
     static var previews: some View {
         PhraseIndexView()
     }
-}
-
-func foo() {
-    let strings = ["One", "Two", "Three"]
-    
-    // More modern way
-    strings.forEach { string in
-        print(string)
-    }
-    
-    // C-style way
-    for string in strings {
-        print(string)
-    }
-    
-    // Example mapping to a constant (modern way of doing this)
-    let mapFoo = strings.map { string in
-        "awesome \(string)"
-    }
-    print(mapFoo)
-   
-    // Best version - shorthand parameter name
-    let mapFoo2 = strings.map { "awesome \($0)" }
-    print(mapFoo2)
-    
-    // Appending with foreach (not preferred)
-    var forEachFoo = [String]()
-    strings.forEach { string in
-        forEachFoo.append("awesome \(string)")
-    }
-    
-    let _ = strings.sorted { (lhs, rhs) -> Bool in
-        lhs < rhs
-    }
-    let _ = strings.sorted { $0 < $1 }
 }
