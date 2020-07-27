3a1b5bf78a63ec1f324af1e8a28f9391f914764f
jro <j@ro.vu>
2020-07-27 11:03:51 -0500

    Issue with Generics (?); Created Collapsed/Expanded view using @State

--- App/Views/LessonIndexView.swift
+++ App/Views/LessonIndexView.swift
@@ -1,29 +1,62 @@
 import SwiftUI
 
-struct LessonIndexView<Repository>: View where Repository: LessonRepository {
-    @EnvironmentObject var lessonRepository: Repository
+struct LessonIndexView: View {
+    @EnvironmentObject var lessonRepository: HardcodedJSONLessonRepository
+    @State var selectedLessonFrequencyRank = -1
 
     var body: some View {
         NavigationView {
             List {
                 ForEach(lessonRepository.allLessons, id: \.id) { lesson in
-                    NavigationLink(destination: LessonView(lesson: lesson)) {
-                        HStack {
-                            Text(lesson.phrase.foreign)
-                            Spacer()
-                            Text(lesson.phrase.native).foregroundColor(.gray)
+                    Button(action: {
+                        self.selectedLessonFrequencyRank = lesson.frequencyRank
+                    }) {
+                        Group {
+                            if lesson.frequencyRank == self.selectedLessonFrequencyRank {
+
+                                LessonView(lesson: lesson)
+                            } else {
+                                CollapsedLessonView(lesson: lesson)
+                            }
                         }
                     }
                 }
             }.navigationBarTitle(Text("Lessons"))
         }
     }
+
+    private struct CollapsedLessonView: View {
+        var lesson: Lesson
+
+        var body: some View {
+            HStack {
+                Text(lesson.phrase.foreign)
+                Spacer()
+                Text(lesson.phrase.native).foregroundColor(.gray)
+            }
+        }
+    }
+
+
+
+
+
 }
 
 
 struct LessonIndexView_Previews: PreviewProvider {
     static var previews: some View {
-        LessonIndexView<HardcodedJSONLessonRepository>()
+        LessonIndexView()
             .environmentObject(HardcodedJSONLessonRepository())
     }
 }
+
+
+
+
+
+
+// TODO:
+// State variable - which lesson is currently selected
+// In the foreach, if lesson matches what is selected, expand/show details
+// Button to toggle it
